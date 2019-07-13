e59938ed67171a33fa7db6626b7e5e07329b8dbe
Kia Teymourian <kiat@bu.edu>
2019-07-13 16:51:21 -0400

    updated pom JDK 1.8

--- src/main/java/edu/bu/met/cs665/Main.java
+++ src/main/java/edu/bu/met/cs665/Main.java
@@ -11,16 +11,16 @@ public class Main {
 
   /**
    * A main method to run examples.
-   * 
+   *
    * @param args not used
    */
   public static void main(String[] args) {
-	  
-    // This configuration is for setting up the log4j properties file. 
-    // It is better to set this using java program arguments. 
+
+    // This configuration is for setting up the log4j properties file.
+    // It is better to set this using java program arguments.
     // PropertyConfigurator.configure("log4j.properties");
-	
-    // Let us create an object of the Main class. 
+
+    // Let us create an object of the Main class.
     Main m = new Main();
 
     logger.info(m.doIt());
