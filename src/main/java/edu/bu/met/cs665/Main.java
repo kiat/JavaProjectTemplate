3338d15accac8b10dcd3bfa56d52b6410399fa3e
Kia Teymourian <kiat@bu.edu>
2021-03-18 14:33:12 -0400

    update log4j

--- src/main/java/edu/bu/met/cs665/Main.java
+++ src/main/java/edu/bu/met/cs665/Main.java
@@ -25,12 +25,12 @@ public class Main {
 
     logger.info(m.doIt());
 
-//    logger.trace("Trace Message!");
-//    logger.debug("Debug Message!");
-//    logger.info("Info Message!");
-//    logger.warn("Warn Message!");
-//    logger.error("Error Message!");
-//    logger.fatal("Fatal Message!");
+    logger.trace("This is a Trace Message for illustration purpose only! This is the highest level which prints all trace, debug, info, warn, error and fatal logs.");
+    logger.debug("This is a Debug Message for illustration purpose only! This log level prints debug, info, warn, error and fatal logs.");
+    logger.info("This is a Info Message for illustration purpose only! This log level prints info, warn, error and fatal logs.");
+    logger.warn("This is a Warn Message for illustration purpose only! This log level prints warn, error and fatal logs.");
+    logger.error("This is a Error Message for illustration purpose only! This log level prints error and fatal logs.");
+    logger.fatal("This is a Fatal Message for illustration purpose only! This log level is the loweest level, and prints only fatal logs.");
 
   }
 
