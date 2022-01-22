94fbb76c75766f974ce9cd9b0459297c4137fb0a
kiat <kiat@cs.utexas.edu>
2022-01-22 00:28:46 -0600

    up

--- run.bat
+++
@@ -1,3 +0,0 @@
-mvn clean compile
-mvn exec:java -Dexec.executable="edu.bu.met.cs665.Main" -Dlog4j.configuration="file:log4j.properties"
-
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mvn clean compile
-mvn exec:java -Dexec.executable="edu.bu.met.cs665.Main" -Dlog4j.configuration="file:log4j.properties"
+mvn exec:java -Dexec.executable="edu.utexas.cs.cs378.Main" -Dexec.args="WikipediaPagesOneDocPerLine.txt.bz2"
